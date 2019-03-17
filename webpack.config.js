9d3613ee780430dd55dc452cef19e069acf47b54
Who am i <joh@ww.com>
2019-03-17 22:08:52 +0300

    add ts  module

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const prod = require('./webpackConfig/webpack.production');
 const dev = require('./webpackConfig/webpack.development')
 const lessLoader = require('./webpackConfig/webpack-less');
 const jsxLoadrer = require('./webpackConfig/webpack-jsx-js');
+const tsLoadrer = require('./webpackConfig/webpack-ts');
 
 module.exports = env => {
   if (env.production === 'build') {    
-    return merge(common, lessLoader, jsxLoadrer, dev)
+    return merge(common, lessLoader, jsxLoadrer, tsLoadrer, dev)
   } 
   
   return merge(common,lessLoader, jsxLoadrer, prod);  
