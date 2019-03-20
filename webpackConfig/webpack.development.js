874226fa250849b5e7fc0fc3d3abd9c750131d01
Who am i <joh@ww.com>
2019-03-20 20:21:45 +0300

    marge webpack files

--- webpackConfig/webpack.development.js
+++ webpackConfig/webpack.development.js
@@ -4,11 +4,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     devServer: {            
-        open: true
+        open: true,
+        proxy: {
+            '*':  'http://localhost:1337',
+        },
+
       },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,                
+                use: ['babel-loader', 'ts-loader'],
+                exclude: /node_modules/                
+            },
+            {
+                test: /\.less$/,                
+                use: extractTextWebpackPlugin.extract({                   
+                    use: ['css-loader', 'less-loader']
+                })
+            },
+        ]
+    }, 
     plugins: [
         new extractTextWebpackPlugin('style/style.css'),
         new HtmlWebpackPlugin({
--- webpackConfig/webpack.production.js
+++ webpackConfig/webpack.production.js
@@ -4,6 +4,21 @@ const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,                
+                use: ['babel-loader', 'ts-loader'],
+                exclude: /node_modules/                
+            },
+            {
+                test: /\.less$/,                
+                use: extractTextWebpackPlugin.extract({                   
+                    use: ['css-loader', 'less-loader']
+                })
+            },
+        ]
+    }, 
     plugins: [
         new htmlWebpackPlugin({
             template: './src/index.html'
