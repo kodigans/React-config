307fa47d9d70c73bd495bca46c26101d46d84205
Who am i <joh@ww.com>
2019-03-21 07:45:34 +0300

    add fetch on '/'

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,59 @@ import * as ReactDOM from 'react-dom';
 
 import { LoggerForm } from './components/LogerForm/LogerForm';
 
+interface ICountry {
+    county: string
+}
+interface IProps {
+    //hits: number
+}
+interface IState {
+    hits: Array<ICountry>;
+}
 
-interface IAppProps {}
-interface IAppState {}
-
-class App extends  React.Component<IAppProps, IAppState> {
-    constructor(props: IAppProps) {
-        super(props);
+class App extends React.Component<{}, IState> {
+    state = {
+        hits: []
     }
 
-    public render() {
+
+    componentDidMount() {
+        const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              country: '1',              
+            }),
+          };
+
+        fetch('/', options)
+            .then(res => res.text())
+            .then(data => JSON.parse(data))
+            .then(arr => {
+                console.log(arr);
+                this.setState({hits: arr});                
+            })
+    }
+    
+
+    public render() {   
+    const {hits} = this.state;     
         return (
-            <>
+            <>            
+      <ul>
+      {hits.map(user => {        
+
+        return (
+          <h1> {`${user.name} ${user.fullName}`} </h1>
+        );
+      })}
+      </ul>
                 <LoggerForm />
             </>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
